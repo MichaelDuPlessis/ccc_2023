cc629697836a41bf471a71bff459774c93fa88dd
Michael du Plessis <mdp0101@gmail.com>
2023-03-31 15:52:44 +0200

    added fight strufct

--- src/fight.rs
+++ src/fight.rs
@@ -9,4 +9,16 @@ impl Fight {
     pub fn new(f1: RPSMove, f2: RPSMove) -> Self {
         Self { f1, f2 }
     }
+
+    pub fn fight(&self) -> &RPSMove {
+        match self.f1.outcome_of_match(&self.f2) {
+            crate::rps::MatchOutcome::Win(_) => &self.f1,
+            crate::rps::MatchOutcome::Draw(_) => &self.f1,
+            crate::rps::MatchOutcome::Loss(_) => &self.f2,
+        }
+    }
+
+    pub fn fight_char(&self) -> char {
+        self.f1.outcome_of_match(&self.f2).get_char()
+    }
 }
