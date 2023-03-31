68bfb7cd04abe098fe6d986d03666f6dc022028f
lastrucci01 <u20430168@tuks.co.za>
2023-03-31 15:38:15 +0200

    splitting on a damn whitespace

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ mod rps;
 
 fn main() {
     for i in 1..=5 {
-        let data = std::fs::read_to_string(format!("./level1/level1_{i}.in")).unwrap();
+        let data = std::fs::read_to_string(format!("./level2/level2_{i}.in")).unwrap();
         let mut lines = data.lines();
-        let num = lines.next().unwrap().parse::<usize>().unwrap();
+        let (n_str, m_str) = lines.next().unwrap().split_once(' ').unwrap();
+        let n = n_str.parse::<usize>().unwrap();
+        let m = m_str.parse::<usize>().unwrap();
+        
+        
 
         let out = lines
             .map(|line| match line {
