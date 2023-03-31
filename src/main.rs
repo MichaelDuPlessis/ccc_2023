aae9cc9af25e049cafadd63a134cbe42dafc5267
lastrucci01 <u20430168@tuks.co.za>
2023-03-31 17:55:49 +0200

    NOT GOOD ENOUGH

--- src/main.rs
+++ src/main.rs
@@ -9,58 +9,34 @@ use tournament::Tournament;
 use tree_attempt::try_solve;
 
 fn main() {
-    // for i in 1..=5 {
-    //     let data = std::fs::read_to_string(format!("./level3/level3_{i}.in")).unwrap();
-    //     // let data = std::fs::read_to_string(format!("./level2/level2_example.in")).unwrap();
-    //     let mut lines = data.lines();
-    //     let (n_str, m_str) = lines.next().unwrap().split_once(' ').unwrap();
-    //     let num_lines = n_str.parse::<usize>().unwrap();
-    //     let num_fighters = m_str.parse::<usize>().unwrap();
+    for i in 1..=5 {
+        let data = std::fs::read_to_string(format!("./level3/level3_{i}.in")).unwrap();
+        // let data = std::fs::read_to_string(format!("./level2/level2_example.in")).unwrap();
+        let mut lines = data.lines();
+        let (n_str, m_str) = lines.next().unwrap().split_once(' ').unwrap();
+        let num_lines = n_str.parse::<usize>().unwrap();
+        let num_fighters = m_str.parse::<usize>().unwrap();
 
-    //     let tournaments = lines
-    //         .map(|line| {
-    //             let splits = line.split(" ").collect::<Vec<&str>>();
-    //             println!("{:?}", splits);
-    //             let r = (splits[0][0..splits[0].len() - 1])
-    //                 .parse::<usize>()
-    //                 .unwrap();
-    //             let p = (splits[1][0..splits[1].len() - 1])
-    //                 .parse::<usize>()
-    //                 .unwrap();
-    //             let s = (splits[2][0..splits[2].len() - 1])
-    //                 .parse::<usize>()
-    //                 .unwrap();
-    //             (r, p, s)
-    //         })
-    //         .collect::<Vec<_>>();
+        let tournaments = lines
+            .map(|line| {
+                let splits = line.split(" ").collect::<Vec<&str>>();
+                let r = (splits[0][0..splits[0].len() - 1])
+                    .parse::<usize>()
+                    .unwrap();
+                let p = (splits[1][0..splits[1].len() - 1])
+                    .parse::<usize>()
+                    .unwrap();
+                let s = (splits[2][0..splits[2].len() - 1])
+                    .parse::<usize>()
+                    .unwrap();
+                (r, p, s)
+            })
+            .collect::<Vec<_>>();
 
-    //     for t in tournaments {
-    //         println!("{:?}", t);
-    //     }
-    // let mut tournaments = lines
-    //     .map(|line| {
-    //         let fighters = line.as_bytes().chunks(2);
-    //         Tournament::new(
-    //             fighters
-    //                 .map(|f| Fight::new(RPSMove::new(f[0] as char), RPSMove::new(f[1] as char)))
-    //                 .collect::<Vec<Fight>>(),
-    //         )
-    //     })
-    //     .collect::<Vec<Tournament>>();
-
-    // for t in &mut tournaments {
-    //     t.torn(2);
-    // }
-
-    // let out = tournaments
-    //     .into_iter()
-    //     .map(|t| format!("{}", t))
-    //     .collect::<Vec<String>>()
-    //     .join("\n");
-
-    // std::fs::write(format!("./level2/level2_{i}.out"), out).unwrap();
-
-    try_solve((4, 3, 1));
+        for t in tournaments {
+            try_solve(t);
+        }
+    }
 }
 
 fn get_win(style: char) -> char {
