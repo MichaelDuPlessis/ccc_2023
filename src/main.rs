8b66e9d4444a81c1125f0e060de77803218185e2
lastrucci01 <u20430168@tuks.co.za>
2023-03-31 16:57:35 +0200

    PARSE

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,54 @@ use tournament::Tournament;
 
 fn main() {
     for i in 1..=5 {
-        let data = std::fs::read_to_string(format!("./level2/level2_{i}.in")).unwrap();
+        let data = std::fs::read_to_string(format!("./level3/level3_{i}.in")).unwrap();
         // let data = std::fs::read_to_string(format!("./level2/level2_example.in")).unwrap();
         let mut lines = data.lines();
         let (n_str, m_str) = lines.next().unwrap().split_once(' ').unwrap();
-        let n = n_str.parse::<usize>().unwrap();
-        let m = m_str.parse::<usize>().unwrap();
+        let num_lines = n_str.parse::<usize>().unwrap();
+        let num_fighters = m_str.parse::<usize>().unwrap();
 
-        let mut tournaments = lines
+        let tournaments = lines
             .map(|line| {
-                let fighters = line.as_bytes().chunks(2);
-                Tournament::new(
-                    fighters
-                        .map(|f| Fight::new(RPSMove::new(f[0] as char), RPSMove::new(f[1] as char)))
-                        .collect::<Vec<Fight>>(),
-                )
+                let splits = line.split(" ").collect::<Vec<&str>>();
+                println!("{:?}", splits);
+                let r = (splits[0][0..splits[0].len() - 1])
+                    .parse::<usize>()
+                    .unwrap();
+                let p = (splits[1][0..splits[1].len() - 1])
+                    .parse::<usize>()
+                    .unwrap();
+                let s = (splits[2][0..splits[2].len() - 1])
+                    .parse::<usize>()
+                    .unwrap();
+                (r, p, s)
             })
-            .collect::<Vec<Tournament>>();
+            .collect::<Vec<_>>();
 
-        for t in &mut tournaments {
-            t.torn(2);
+        for t in tournaments {
+            println!("{:?}", t);
         }
+        // let mut tournaments = lines
+        //     .map(|line| {
+        //         let fighters = line.as_bytes().chunks(2);
+        //         Tournament::new(
+        //             fighters
+        //                 .map(|f| Fight::new(RPSMove::new(f[0] as char), RPSMove::new(f[1] as char)))
+        //                 .collect::<Vec<Fight>>(),
+        //         )
+        //     })
+        //     .collect::<Vec<Tournament>>();
 
-        let out = tournaments
-            .into_iter()
-            .map(|t| format!("{}", t))
-            .collect::<Vec<String>>()
-            .join("\n");
+        // for t in &mut tournaments {
+        //     t.torn(2);
+        // }
 
-        std::fs::write(format!("./level2/level2_{i}.out"), out).unwrap();
+        // let out = tournaments
+        //     .into_iter()
+        //     .map(|t| format!("{}", t))
+        //     .collect::<Vec<String>>()
+        //     .join("\n");
+
+        // std::fs::write(format!("./level2/level2_{i}.out"), out).unwrap();
     }
 }
